a965afbaf578d3b77c940b5fe1f923a6c21c1a99
ZzThanhBaozZ <143920805+TranDatDev@users.noreply.github.com>
2023-11-10 17:43:42 +0700

    Frontend cycle7

--- Frontend/peims/src/components/LandingPage/LandingFooter/LandingFooter.jsx
+++ Frontend/peims/src/components/LandingPage/LandingFooter/LandingFooter.jsx
@@ -1,32 +1,88 @@
 import "./LandingFooter.scss";
+import { Link } from "react-router-dom";
+import DarkLogo from "../../../assets/DarkPeims.svg";
+import LightLogo from "../../../assets/LightPeims.svg";
 const LandingFooter = () => {
     return (
         <>
             <section className="footer__container is__desktop">
                 <div className="footer__part--left">
                     <article className="footer__web__name">
-                        <h1 className="footer__short__name"></h1>
-                        <p className="footer__full__name">a</p>
+                        <h1 className="footer__short__name">PEIMS</h1>
+                        <p className="footer__full__name">
+                            Personal Expenditure Integrity Management System
+                        </p>
                     </article>
-                    <div className="footer__web__address">
-                        <p></p>
+                    <div className="footer__web__address footer__desktop">
+                        <p>
+                            207 Giai Phong Street, Dong Tam Ward, Hai Ba Trung
+                            District, Ha Noi.
+                        </p>
                     </div>
                 </div>
-                <div className="footer__part--center">
+                <div className="footer__part--center footer__desktop">
                     <div className="footer__nav--first">
                         <nav>
                             <p>Links</p>
                             <ul>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/products">Products</Link>
+                                </li>
+                                <li>
+                                    <Link to="/pricing">Pricing</Link>
+                                </li>
+                                <li>
+                                    {" "}
+                                    <Link to="/about">About us</Link>
+                                </li>
                             </ul>
                         </nav>
                     </div>
-                    <div className="footer__nav--second"></div>
+                    <div className="footer__nav--second footer__desktop">
+                        <nav>
+                            <p>Help</p>
+                            <ul>
+                                <li>
+                                    <Link to="/">Payment</Link>
+                                </li>
+                                <li>
+                                    <Link to="/">Return</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-                <div className="footer__part--right">c</div>
+                <div className="footer__part--right">
+                    <div className="footer__newsletter footer__desktop">
+                        <h1>Newsletter</h1>
+                        <div className="footer__sending">
+                            <input
+                                type="text"
+                                placeholder="Enter Your Email Address"
+                            />
+                            <div className="footer__subscribe">
+                                <h5>SUBSCRIBE</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer__copyright">
+                        <img
+                            src={LightLogo}
+                            alt=""
+                            className="footer__logo is__dark"
+                        />
+                        <img
+                            src={DarkLogo}
+                            alt=""
+                            className="footer__logo is__light"
+                        />
+                        <small>Terms of service</small>
+                        <h2>Copyright © 2023 PEIMS</h2>
+                    </div>
+                </div>
             </section>
         </>
     );
--- Frontend/peims/src/components/LandingPage/LandingHeader/LandingHeader.jsx
+++ Frontend/peims/src/components/LandingPage/LandingHeader/LandingHeader.jsx
@@ -8,78 +8,80 @@ import DarkModeButton from "../../DarkModeButton";
 // eslint-disable-next-line react/prop-types
 
 const LandingHeader = ({ isDarkMode, toggleDarkMode }) => {
-   return (
-      <>
-         <header>
-            <div className="landing__header__container">
-               <section className="landing__logo__title">
-                  <img
-                     src={LightLogo}
-                     alt=""
-                     className="landing__logo is__light"
-                  />
-                  <img
-                     src={DarkLogo}
-                     alt=""
-                     className="landing__logo is__dark"
-                  />
-                  <p className="landing__logo__title is__desktop">PEIMS</p>
-               </section>
-               <nav className="landing__nav--mobile is__mobile">
-                  <Link to="/login">login</Link>
-                  <Link to="/register">register</Link>
-                  <DarkModeButton
-                     isDarkMode={isDarkMode}
-                     toggleDarkMode={toggleDarkMode}
-                  />
-                  <Link to="/">
-                     <img src={home} alt="a" />
-                  </Link>
-                  <Link to="/products">
-                     <img src={utils} alt="b" />
-                  </Link>
-                  <Link to="/pricing">
-                     <img src="" alt="c" />
-                  </Link>
-                  <Link to="/about">
-                     <img src="" alt="c" />
-                  </Link>
-                  {/* <div className="landing__login--btn">
+    return (
+        <>
+            <header>
+                <div className="landing__header__container">
+                    <section className="landing__logo__title">
+                        <img
+                            src={LightLogo}
+                            alt=""
+                            className="landing__logo is__light"
+                        />
+                        <img
+                            src={DarkLogo}
+                            alt=""
+                            className="landing__logo is__dark"
+                        />
+                        <p className="landing__logo__title is__desktop">
+                            PEIMS
+                        </p>
+                    </section>
+                    <nav className="landing__nav--mobile is__mobile">
+                        <Link to="/login">login</Link>
+                        <Link to="/register">register</Link>
+                        <DarkModeButton
+                            isDarkMode={isDarkMode}
+                            toggleDarkMode={toggleDarkMode}
+                        />
+                        <Link to="/">
+                            <img src={home} alt="a" />
+                        </Link>
+                        <Link to="/products">
+                            <img src={utils} alt="b" />
+                        </Link>
+                        <Link to="/pricing">
+                            <img src="" alt="c" />
+                        </Link>
+                        <Link to="/about">
+                            <img src="" alt="c" />
+                        </Link>
+                        {/* <div className="landing__login--btn">
                             <p>Đăng nhập</p>
                         </div> */}
-               </nav>
-               <nav className="landing__nav--desktop is__desktop">
-                  <ul>
-                     <li className="landing__link">
-                        <Link to="/">Trang chủ</Link>
-                     </li>
-                     <li>
-                        <Link to="/products">Sản Phẩm</Link>
-                     </li>
-                     <li>
-                        <Link to="/pricing">Bảng Giá</Link>
-                     </li>
-                     <li>
-                        <Link to="/about">Thông Tin</Link>
-                     </li>
-                  </ul>
-               </nav>
-               <div className="landing__login is__desktop">
-                  <DarkModeButton
-                     isDarkMode={isDarkMode}
-                     toggleDarkMode={toggleDarkMode}
-                  />
-                  <button className="landing__login--btn">
-                     <Link to="/login">Đăng Nhập</Link>
-                  </button>
-                  <button className="landing__register--btn">
-                     <Link to="/register">Đăng Ký</Link>
-                  </button>
-               </div>
-            </div>
-         </header>
-      </>
-   );
+                    </nav>
+                    <nav className="landing__nav--desktop is__desktop">
+                        <ul>
+                            <li className="landing__link">
+                                <Link to="/">Trang chủ</Link>
+                            </li>
+                            <li>
+                                <Link to="/products">Sản Phẩm</Link>
+                            </li>
+                            <li>
+                                <Link to="/pricing">Bảng Giá</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">Thông Tin</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="landing__login is__desktop">
+                        <DarkModeButton
+                            isDarkMode={isDarkMode}
+                            toggleDarkMode={toggleDarkMode}
+                        />
+                        <button className="landing__login--btn">
+                            <Link to="/login">Đăng Nhập</Link>
+                        </button>
+                        <button className="landing__register--btn">
+                            <Link to="/register">Đăng Ký</Link>
+                        </button>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default LandingHeader;
